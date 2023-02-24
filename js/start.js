b2dc66d4b83d86df96e0a650d6586b4f3ff978da
Comf2 <otischaka4@gmail.com>
2023-02-23 22:20:39 -0500

    Front End

---
+++ js/start.js
@@ -0,0 +1,37 @@
+// slider
+const pickSymSlider = document.querySelector('.pick-sym-slider');
+
+let sliderOnX = true;
+
+const xSym = document.querySelector('#x-sym');
+const oSym = document.querySelector('#o-sym');
+
+xSym.onclick = () => {
+	sliderOnX = true;
+	checkSlider();
+};
+oSym.onclick = () => {
+	sliderOnX = false;
+	checkSlider();
+};
+//make it not toggle
+function checkSlider() {
+	if (sliderOnX) {
+		pickSymSlider.style.setProperty('--slider-position', '0');
+		oSym.style.color = '#a8bfc9';
+		xSym.style.color = '#11242e';
+	}
+	if (!sliderOnX) {
+		pickSymSlider.style.setProperty('--slider-position', '50%');
+		xSym.style.color = '#a8bfc9';
+		oSym.style.color = '#11242e';
+	}
+}
+
+//init game
+const newCpuGame = document.querySelector('.new-game-cpu');
+newCpuGame.onclick = () => initCpuGame();
+function initCpuGame() {
+	let team;
+	sliderOnX ? (team = 'x') : (team = 'o');
+}
